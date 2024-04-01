3661410fb063dce4018c58e27bd492ed4c83d282
Aluno EDU FOR 2 <alunofor2@edu.sesc-ce.com.br>
2024-04-01 09:26:09 -0300

    repositorio da nave

---
+++ scripts/lazer.gd
@@ -0,0 +1,16 @@
+extends Node2D  
+
+var vel = 200 
+
+func _ready(): 
+	  pass 
+
+
+func _process(delta): 
+	   translate(Vector2(0  , -1) * vel * delta)
+	
+	
+func _on_visibilityNotifier2D_screen_exited():
+	 queue_free()
+	
+			  
---
+++ scripts/ship.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+const PRE_LAZER = preload("res://scenes/lazer.tscn")
+
+var vel = 100
+
+#posicao mínima esquerda 21
+#posicao máxima direita 139
+
+func _ready():
+	pass 
+	
+
+func _process(delta):
+	
+	 
+	var dirX = 0
+	var dirY = 0
+	
+	if Input.is_action_pressed("ui_left"): 
+		dirX += -1
+		
+	if Input.is_action_pressed("ui_right"):
+		dirX += 1
+		
+	if Input.is_action_pressed("ui_up"):
+		dirY += -1
+		
+	if Input.is_action_pressed("ui_down"):
+		dirY += 1
+		
+	if Input.is_action_just_pressed("ui_accept"):
+		if  get_tree().get_nodes_in_group("lazeres").size() < 3:
+			var lazer = PRE_LAZER.instance()
+			get_parent().add_child(lazer)
+			lazer.global_position = $blaster.global_position 
+		
+		
+	translate(Vector2(dirX , dirY) * vel * delta)
+	
+	global_position.x = clamp(global_position.x , 21 , 139)
+	global_position.y = clamp(global_position.y , 24 , 266)
+  
